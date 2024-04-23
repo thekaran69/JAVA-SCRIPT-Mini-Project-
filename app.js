e7fa0c0f7bde9e88e0e2df5f866ddf4e12264c63
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-04-23 22:34:17 +0530

    function added

---
+++ app.js
@@ -0,0 +1,42 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "green","purple"];
+
+let started  = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress" ,function(){
+    if(started == false){
+
+        console.log("game stared");
+        started == true;
+
+        levelUp();
+
+    }
+});
+
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 350)
+
+}
+
+function levelUp(){
+    level++;
+    h2.innerText =`Level ${level}`;
+
+    let randIdx  = Math.floor(Math.random() *3);
+    let randColor = btns[randIdx];
+    let randBtn  = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
